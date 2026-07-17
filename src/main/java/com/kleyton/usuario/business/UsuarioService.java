7037e5da6e49dc724dd339591f819cf55af80eca
kleytons1989 <kleytons1989@gmail.com>
2026-07-17 06:07:52 -0300

    get delete

--- src/main/java/com/kleyton/usuario/business/UsuarioService.java
+++ src/main/java/com/kleyton/usuario/business/UsuarioService.java
@@ -4,6 +4,7 @@ import com.kleyton.usuario.business.converter.UsuarioConverter;
 import com.kleyton.usuario.business.dto.UsuarioDTO;
 import com.kleyton.usuario.infrastructure.entity.Usuario;
 import com.kleyton.usuario.infrastructure.exceptions.ConflictException;
+import com.kleyton.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.kleyton.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -42,6 +43,16 @@ public class UsuarioService {
 
     public boolean verificaEmailExistente(String email){
         return usuarioRepository.existsByEmail(email);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email){
+        return usuarioRepository.findByEmail(email).orElseThrow(
+                () -> new ResourceNotFoundException("Email não encontrado" + email));
+    }
+
+    public void deletaUsuarioPorEmail(String email){
+        usuarioRepository.deleteByEmail(email);
+
     }
 
 }
