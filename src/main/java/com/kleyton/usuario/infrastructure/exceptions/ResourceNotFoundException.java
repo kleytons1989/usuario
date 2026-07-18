da1bc029825f4ec119bac1f31f4c642f5171248e
kleytons1989 <kleytons1989@gmail.com>
2026-07-18 04:17:07 -0300

    Primeiro commit get e delete

---
+++ src/main/java/com/kleyton/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.kleyton.usuario.infrastructure.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String mensagem){
+        super(mensagem);
+    }
+
+    public ResourceNotFoundException(String mensagem, Throwable throwable){
+        super(mensagem, throwable);
+    }
+}
---
+++ src/main/java/com/kleyton/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,11 @@
+package com.kleyton.usuario.infrastructure.repository;
+
+
+import com.kleyton.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
